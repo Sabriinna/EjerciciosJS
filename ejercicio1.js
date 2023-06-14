9447f02a21aea0718390dccdc0423041196e6113
UsuarioM <sabrinasimoncoelho@gmail.com>
2023-06-14 09:07:10 +0200

    subida de ejercicios

---
+++ ejercicio1.js
@@ -0,0 +1,14 @@
+//1.1
+let myFavoriteHero = "Hulk";
+console.log(myFavoriteHero);
+//1.2
+let x = 50;
+console.log(x);
+//1.3
+let h = 5;
+console.log(h);
+let y = 10;
+console.log(y);
+//1.4
+let z = h+y;
+console.log(h+y);
---
+++ ejrcicio4.js
@@ -0,0 +1,17 @@
+//1.1
+const avengers = ["HULK","SPIDERMAN","BLACK PANTHER"];
+console.log(avengers[0]);
+//1.2
+avengers[0] = "IRONMAN";
+//1.3
+console.log(avengers.length);
+//1.4
+const rickAndMortyCharacteres = ["RICK","BETH","JERRY"];
+rickAndMortyCharactere.push("MORTY", "SUMMER");
+console.log( rickAndMortyCharacteres[rickAndMortyCharacteres.lenght - 1]);
+//1.5
+rickAndMortyCharacteres.pop();
+console.log(rickAndMortyCharacteres[0],rickAndMortyCharacteres^[rickAndMortyCharacteres.lenght - 1]);
+//1.6
+rickAndMortyCharacteres.splice(1, 1);
+console.log(rickAndMortyCharacteres);
